0a5f986d3e9bbeea7910821857a33617d3293b14
lirik <kirill.isachenko@gmail.com>
2024-06-17 21:34:31 +0700

    feat: Add .browserslistrc, postcss.config.cjs, and dot.svg files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://lirik.pro',
-})
+  renderers: ['@astrojs/renderer-postcss'],
+  integrations: [icon()]
+});
