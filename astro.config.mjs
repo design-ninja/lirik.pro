fe9262f27ed8777df53c47b0804074c06ae1869e
lirik <kirill.isachenko@gmail.com>
2024-06-22 09:25:53 +0700

    add astrojs-service-worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
-
 import critters from "astro-critters";
+import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lirik.pro',
-  integrations: [icon(), sitemap(), robots(), critters()],
+  integrations: [icon(), sitemap(), robots(), critters(), serviceWorker()],
   renderers: ['@astrojs/renderer-postcss']
 });
