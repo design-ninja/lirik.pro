f2e5589a007a97f1c5e63e5f292993cf8f63bbc9
lirik <kirill.isachenko@gmail.com>
2024-06-22 08:29:57 +0700

    add criters

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import icon from "astro-icon";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lirik.pro',
-  integrations: [icon(), sitemap(), robots()],
+  integrations: [icon(), sitemap(), robots(), critters()],
   renderers: ['@astrojs/renderer-postcss']
 });
