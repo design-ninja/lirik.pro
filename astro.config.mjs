c2fe6536a6526e35aec5866770f9e8e869fc3cb0
lirik <kirill.isachenko@gmail.com>
2024-06-23 15:42:58 +0700

    remove astro-critters and video logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
-import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lirik.pro',
-  integrations: [icon(), sitemap(), robots(), critters()],
+  integrations: [icon(), sitemap(), robots()],
   renderers: ['@astrojs/renderer-postcss']
 });
