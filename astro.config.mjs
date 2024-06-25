6334f7498a8d6ca2a5bcb8bf615d7343f2df6810
lirik <kirill.isachenko@gmail.com>
2024-06-25 20:52:12 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lirik.pro',
-  integrations: [icon(), sitemap(), robots(), partytown()],
+  integrations: [icon(), sitemap(), robots()],
   renderers: ['@astrojs/renderer-postcss']
 });
