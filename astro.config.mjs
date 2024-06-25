e355632c5028d9eb9c50a0fcb56320255077ddd0
lirik <kirill.isachenko@gmail.com>
2024-06-25 18:00:25 +0700

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lirik.pro',
-  integrations: [icon(), sitemap(), robots()],
+  integrations: [icon(), sitemap(), robots(), partytown()],
   renderers: ['@astrojs/renderer-postcss']
 });
