cec208fb9c8116b8b829efb867cab267cf58a99e
lirik <kirill.isachenko@gmail.com>
2025-10-25 19:42:50 +0700

    chore: configure Netlify adapter and update project settings
    - Added Netlify adapter to the Astro configuration for deployment.
    - Updated .gitignore to exclude the local Netlify folder.
    - Modified package.json to include the Netlify adapter as a dependency.
    - Updated pnpm-lock.yaml to reflect changes in dependencies.
    - Updated state.json with new site ID and geolocation data.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://lirik.pro',
+  output: 'server',
+  adapter: netlify(),
   integrations: [icon(), mdx(), sitemap()],
   renderers: ['@astrojs/renderer-postcss']
 });
