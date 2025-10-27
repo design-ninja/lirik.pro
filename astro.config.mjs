5ff2f765e2b6768b962d1908c0f6ccd1b52df7de
lirik <kirill.isachenko@gmail.com>
2025-10-27 10:48:16 +0700

    refactor: update Astro configuration and remove unused Spotify API endpoints
    - Changed the output setting in the Astro configuration from 'server' to 'static' for improved deployment.
    - Removed the Netlify adapter from dependencies as it is no longer needed.
    - Added a new GitHub Actions workflow to update Gist data for Spotify and Wakatime.
    - Refactored the InfoTable component to fetch Spotify and Wakatime data from environment variables instead of local API endpoints.
    - Deleted obsolete Spotify callback and data fetching API files to streamline the codebase.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://lirik.pro',
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
   integrations: [icon(), mdx(), sitemap()]
 });
